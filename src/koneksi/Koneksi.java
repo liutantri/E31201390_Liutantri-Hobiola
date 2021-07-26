5614a61e74ce6c7ed2ff84f984c3241a4c991ef3
Asus <Asus@LAPTOP-0DTR71A0>
2021-07-26 20:19:44 +0700

    final project_liutantri hobiola_E31201390_MifC

---
+++ src/koneksi/Koneksi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Koneksi {
+    private static Connection koneksi;
+    
+      public static Connection GetConnection()throws SQLException{
+          if (koneksi == null){
+              new Driver();
+              
+              koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/"
+                      + "gudang","root","");
+          }
+          return koneksi;
+      }
+}
